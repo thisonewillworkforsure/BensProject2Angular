bc80056f95fba912cc45e2385db93e5c770f514c
thisonewillworkforsure <ben6228@yahoo.com>
2022-09-25 23:59:06 -0400

    implemented register

--- src/app/users/register-user/register-user.component.ts
+++ src/app/users/register-user/register-user.component.ts
@@ -4,7 +4,7 @@ import { UsersService } from '../users.service';
 import { Router } from '@angular/router';
 import { FormGroup } from '@angular/forms';
 import { ConfirmedValidator } from './confirmed.validator';
-import { Validators,FormControl,FormBuilder } from '@angular/forms';
+import { Validators, FormControl, FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-register-user',
   templateUrl: './register-user.component.html',
@@ -12,7 +12,7 @@ import { Validators,FormControl,FormBuilder } from '@angular/forms';
 })
 export class RegisterUserComponent implements OnInit {
 
-  newUser : UserModel = {
+  newUser: UserModel = {
     userID: 0,
     typeID: 0,
     userName: "",
@@ -20,59 +20,65 @@ export class RegisterUserComponent implements OnInit {
     statusID: 1
   }
 
-  
-  reactiveForm : FormGroup = new FormGroup({});
-  passwordCheck : string = "";
+
+  reactiveForm: FormGroup = new FormGroup({});
+  passwordCheck: string = "";
 
   constructor(private userService: UsersService,
     private router: Router,
-    private FB:FormBuilder) {
-      this.reactiveForm = FB.group({
-        username: new FormControl('', Validators.compose([
-          Validators.required,Validators.minLength(6)
-        ])),
-        password: new FormControl('', Validators.required),
-        conpassword: new FormControl('', Validators.required)
-      },
+    private FB: FormBuilder) {
+    this.reactiveForm = FB.group({
+      username: new FormControl('', Validators.compose([
+        Validators.required, Validators.minLength(6)
+      ])),
+      password: new FormControl('', Validators.required),
+      conpassword: new FormControl('', Validators.required)
+    },
       {
-        validators:this.mustMatch('password','conpassword')
+        validators: this.mustMatch('password', 'conpassword')
       })
 
-     }
+  }
 
 
-     get f(){
-      return this.reactiveForm.controls;
-     }
+  get f() {
+    return this.reactiveForm.controls;
+  }
 
-     mustMatch(password:any,conpassword:any){
+  mustMatch(password: any, conpassword: any) {
 
-      return (formGroup:FormGroup)=>{
-        const passwordcontrol = formGroup.controls[password];
-        const conpasswordcontrol = formGroup.controls[conpassword];
+    return (formGroup: FormGroup) => {
+      const passwordcontrol = formGroup.controls[password];
+      const conpasswordcontrol = formGroup.controls[conpassword];
 
-        if(conpasswordcontrol.errors && !conpasswordcontrol.errors['mustMatch']){
-          return; //this is if it has an error not from must match
-        }
+      if (conpasswordcontrol.errors && !conpasswordcontrol.errors['mustMatch']) {
+        return; //this is if it has an error not from must match
+      }
 
-        if(passwordcontrol.value!==conpasswordcontrol.value){
-          conpasswordcontrol.setErrors({mustMatch:true});
-        }
-        else{
-          conpasswordcontrol.setErrors(null);
-        }
-      };
-     }
+      if (passwordcontrol.value !== conpasswordcontrol.value) {
+        conpasswordcontrol.setErrors({ mustMatch: true });
+      }
+      else {
+        conpasswordcontrol.setErrors(null);
+      }
+    };
+  }
 
 
   ngOnInit(): void {
   }
 
-  createUser(): void{
-      this.userService.addUser(this.newUser).subscribe((Response)=>{
-      this.router.navigate(["login-user"]);
+  createUser(): void {
+    this.newUser.userName = this.f['username'].value;
+    this.newUser.userPassword = this.f['password'].value;
+    if (this.newUser.userName != "") {
+      this.newUser.statusID = 1;
+      this.newUser.typeID = 2;
+      this.userService.addUser(this.newUser).subscribe((Response) => {
+        this.router.navigate(["login-user"]);
       })
+    }
   }
 
-  
+
 }
