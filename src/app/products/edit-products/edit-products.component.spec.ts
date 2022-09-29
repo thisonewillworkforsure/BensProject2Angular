3ccf3b3eb840f19a906f1fad308cc686604e09e6
thisonewillworkforsure <ben6228@yahoo.com>
2022-09-29 15:16:09 -0400

    did User Login testing

--- src/app/products/edit-products/edit-products.component.spec.ts
+++ src/app/products/edit-products/edit-products.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ActivatedRoute } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { EditProductsComponent } from './edit-products.component';
 
 describe('EditProductsComponent', () => {
@@ -8,7 +9,20 @@ describe('EditProductsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ EditProductsComponent ]
+      declarations: [ EditProductsComponent ],
+      imports: [FormsModule],
+      providers: [ {
+        provide: ActivatedRoute,
+        useValue: {
+            snapshot: {
+                paramMap: {
+                    get(): number {
+                        return 1;
+                    },
+                },
+            },
+        },
+    },]
     })
     .compileComponents();
 
@@ -17,7 +31,7 @@ describe('EditProductsComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create but I don't use this component but I had to include the modules anyway because I am too afraid of deleting stuff so close to the dealine", () => {
     expect(component).toBeTruthy();
   });
 });
--- src/app/users/login-user/login-user.component.spec.ts
+++ src/app/users/login-user/login-user.component.spec.ts
@@ -1,14 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginUserComponent } from './login-user.component';
-
-describe('LoginUserComponent', () => {
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+fdescribe('LoginUserComponent', () => {
   let component: LoginUserComponent;
   let fixture: ComponentFixture<LoginUserComponent>;
-
+  let httpMock : HttpTestingController
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginUserComponent ]
+      declarations: [ LoginUserComponent ],
+      imports: [HttpClientTestingModule],
+      providers:[HttpTestingController]
     })
     .compileComponents();
 
@@ -19,5 +21,15 @@ describe('LoginUserComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should Have LOGIN displayed', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h2')?.textContent).toContain("LOGIN");
+  });
+
+  it("should Have a button that says 'Forgot Password?'", () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.d-grid .btn')?.textContent).toContain("Forgot Password");
   });
 });
