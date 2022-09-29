3738b36f0f74e8be0f671f4aa09a77f3b6103ff5
thisonewillworkforsure <ben6228@yahoo.com>
2022-09-29 14:28:51 -0400

    product-http-service done

--- src/app/products-http/products-http.service.spec.ts
+++ src/app/products-http/products-http.service.spec.ts
@@ -1,16 +1,46 @@
 import { TestBed } from '@angular/core/testing';
-
+import { ProductModel } from '../products/products.model';
 import { ProductsHttpService } from './products-http.service';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 describe('ProductsHttpService', () => {
   let service: ProductsHttpService;
-
+  let httpMock: HttpTestingController;
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports:[HttpClientTestingModule]
+    });
     service = TestBed.inject(ProductsHttpService);
+    httpMock = TestBed.get(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should give a profuct Model', ()=> {
+    let names : ProductModel[] = [];
+    service.getAllProducts().subscribe((Response)=>{
+      names = Response;
+    });
+    const req = httpMock.expectOne(service.baseUrl);
+    expect(req.request.method).toEqual("GET");
+    //spyOn(service,"getProfile").and.returnValue(of());
+  });
+
+  it('should give a Profile Model', ()=> {
+    let name : ProductModel = {
+      productID : 7,
+      productName: "",
+      productDesc: "",
+      productImg: "",
+      productCost: 7
+
+    };
+    service.getOneProduct(7).subscribe((Response)=>{
+      name = Response;
+    });
+    const req = httpMock.expectOne(service.baseUrl+"/"+name.productID);
+    expect(req.request.method).toEqual("GET");
+    
   });
 });
