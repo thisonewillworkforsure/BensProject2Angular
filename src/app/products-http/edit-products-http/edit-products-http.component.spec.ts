14f3c04fda68ee42b6e16a1b2fbd7b171c7895b8
thisonewillworkforsure <ben6228@yahoo.com>
2022-09-29 15:52:26 -0400

    done with edit products

--- src/app/products-http/edit-products-http/edit-products-http.component.spec.ts
+++ src/app/products-http/edit-products-http/edit-products-http.component.spec.ts
@@ -1,23 +1,49 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EditProductsHttpComponent } from './edit-products-http.component';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { ActivatedRoute } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
-describe('EditProductsHttpComponent', () => {
+fdescribe('EditProductsHttpComponent', () => {
   let component: EditProductsHttpComponent;
   let fixture: ComponentFixture<EditProductsHttpComponent>;
-
+  let httpMock : HttpTestingController
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ EditProductsHttpComponent ]
+      declarations: [ EditProductsHttpComponent,],
+      imports: [HttpClientTestingModule, FormsModule],
+      providers: [{
+        provide: ActivatedRoute,
+        useValue: {
+            snapshot: {
+                paramMap: {
+                    get(): number {
+                        return 1;
+                    },
+                },
+            },
+        },
+    },]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(EditProductsHttpComponent);
+    httpMock = TestBed.inject(HttpTestingController);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have an initial product ID of 0', ()=>{
+    expect(component.updProduct.productID).toEqual(0);
+  });
+
+  it('should have Update Product as the title', ()=>{
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector(".card-title")?.innerHTML).toContain("Update Product");
   });
 });
